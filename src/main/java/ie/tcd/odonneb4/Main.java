549fea994006d4a2f64dc3afc4f96f0f158e3f56
Bríd O'Donnell <odonneb4@tcd.ie>
2022-10-18 18:18:03 +0100

    Added imports for correct_qrel()

--- src/main/java/ie/tcd/odonneb4/Main.java
+++ src/main/java/ie/tcd/odonneb4/Main.java
@@ -1,5 +1,14 @@
 package ie.tcd.odonneb4;
 
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.lucene.queryparser.classic.ParseException;
 import java.io.IOException;
